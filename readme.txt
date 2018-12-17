54eb3412a0768bfa370d5ad4345d3253da2dbeb4
Nesterov Max <braams@braams.ru>
2018-12-17 10:27:13 +0300

    Add dummy function

--- readme.txt
+++ readme.txt
@@ -9,3 +9,11 @@ make install
 
 
 asterisk -rx "module load res_dummy.so"
+
+
+[dummy]
+exten => 100,1,Dummy(arguments)
+same => n,Set(DUMMY(some)=other)
+same => n,NoOp(${DUMMY(some)})
+
+console dial 100@dummy
--- res_dummy.c
+++ res_dummy.c
@@ -3,9 +3,14 @@
 ***/
 
 #include "asterisk.h"
+
 ASTERISK_FILE_VERSION(__FILE__, "$Revision: 1 $")
-#include <asterisk/module.h>
+
+#include "asterisk/module.h"
 #include "asterisk/logger.h"
+#include "asterisk/pbx.h"
+#include "asterisk/xmldoc.h"
+
 
 #define AST_MODULE "app_dummy"
 
@@ -16,24 +21,52 @@ static char *des_dummy = "Description. Dummy application. Do nothing\n";
 
 
 static int app_dummy(struct ast_channel *chan, const char *data) {
-
+    ast_log(LOG_NOTICE, "Dummy App: %s\n", data);
 }
 
 
+static int func_dummy_read(struct ast_channel *chan, const char *cmd, char *data,
+                           char *buf, size_t len) {
+    ast_log(LOG_NOTICE, "Dummy func read: %s = %s", data, buf);
 
-static int load_module(void)
-{
+    return 0;
+}
+
+static int func_dummy_write(struct ast_channel *chan, const char *cmd, char *data, const char *value) {
+    ast_log(LOG_NOTICE, "Dummy func write: %s = %s\n", data, value);
+
+    return 0;
+}
+
+
+static struct ast_custom_function dummy_function = {
+        .name = "DUMMY",
+        .read = func_dummy_read,
+        .write=func_dummy_write,
+        .read_max = 33,
+        .desc="Description here",
+        .synopsis="Synopsis hele",
+        .syntax="Syntax here",
+        .arguments="Arguments here",
+        .seealso="See also here",
+        .docsrc=1,
+
+
+};
+
+static int load_module(void) {
     int res;
     ast_log(LOG_NOTICE, "Hello Dummy\n");
-    res=ast_register_application(name_dummy, app_dummy, syn_dummy, des_dummy);
+    res = ast_register_application(name_dummy, app_dummy, syn_dummy, des_dummy);
+    res = ast_custom_function_register(&dummy_function);
     return AST_MODULE_LOAD_SUCCESS;
 }
 
-static int unload_module(void)
-{
+static int unload_module(void) {
     int res;
     ast_log(LOG_NOTICE, "Goodbye Dummy\n");
     res = ast_unregister_application(name_dummy);
+    res = ast_custom_function_unregister(&dummy_function);
     return AST_MODULE_LOAD_SUCCESS;
 }
 
